2247c0b75af68a2a3483689fd5b4c8583ce60e96
JuanCCarruyo <juanccarruyo@gmail.com>
2020-08-06 19:27:27 -0300

    act

--- dos.js
+++ dos.js
@@ -13,51 +13,51 @@ Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas
 function mostrar()
 {
 	
-	
-	let art;
-	let i=0;
+	//let i=0;
 	let x="si";
 	let nom;
 	let cat;
 	let prec=0;
-	let art;
+	let max=0
+	let txtmax;
+	let txtmaxlac;
 	let cantBeb=0;
 
 	while(x == "si"){ 
 
 		nom = prompt("Ingrese el nombre de un artículo: ");
-		cat = prompt("Ingrese su categoria: ")toLowerCase();
+		cat = prompt("Ingrese su categoria: ").toLowerCase();
 		prec = parseInt(prompt("Ingrese su precio: "));
 
-		i = i+1;
-		art = nom +" "+cat+" "+prec;
+		//i = i+1;
 
 		if (!(cat == "almacen" || cat == "lacteos" || cat == "lácteos" || cat == "limpieza" || cat == "bebidas")){
-			cat = prompt("Ingrese una categoria valida: (almacen, lácteos, limpieza o bebidas)")
+			alert("Debe ingresar una categoria valida: (almacen, lácteos, limpieza o bebidas)");
 		}
 		else {
-			if (prec < 0 || prec > 1000){
-				prec = parseInt(prompt("Ingrese un precio valido: (no puede ser menor a $0 ni mayor a $1000)"));
-			}
-			else {
-				prec = precMax > 
-				if (cat == "bebidas"){
-					cantBeb = cantBeb + 1;
+			if (prec >= 0 && prec <= 1000){
+				if (prec > max) {
+					max = prec;
+					txtmax = "El artículo con mayor precio es: "+nom;
+					if (cat == "lacteos" || cat == "lácteos"){
+						txtmaxlac = "El artículo con mayor precio de la categoría lácteos es: "+nom;
+					}
 				}
+				else {
+				alert("Debe ingresar un precio valido: (no puede ser menor a $0 ni mayor a $1000)");
+			}
+			if (cat == "bebidas"){
+				cantBeb = cantBeb + 1;
+			}
 			}
 		}
-
-
-
 
 		x = prompt("Quieres introducir otro artículo?").toLowerCase();
 
 	}
 
-
-
-	document.write("El artículo con mayor precio es: ");
-	document.write("El artículo con mayor precio de la categoría lácteos es: ");
-	document.write("La cantidad de artículos ingresados de categoría bebidas es: ");
+	document.write(txtmax+"<br>");
+	document.write(txtmaxlac+"<br>");
+	document.write("La cantidad de artículos ingresados de categoría bebidas es: "+cantBeb);
 
 }
