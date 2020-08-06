577338bfb589e6640a3ab486c7f4a3125fccb24d
Juan Carruyo <juanccarruyo@gmail.com>
2020-08-05 22:22:08 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -12,5 +12,52 @@ Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas
 
 function mostrar()
 {
-	alert("Hola Mundo");
+	
+	
+	let art;
+	let i=0;
+	let x="si";
+	let nom;
+	let cat;
+	let prec=0;
+	let art;
+	let cantBeb=0;
+
+	while(x == "si"){ 
+
+		nom = prompt("Ingrese el nombre de un artículo: ");
+		cat = prompt("Ingrese su categoria: ")toLowerCase();
+		prec = parseInt(prompt("Ingrese su precio: "));
+
+		i = i+1;
+		art = nom +" "+cat+" "+prec;
+
+		if (!(cat == "almacen" || cat == "lacteos" || cat == "lácteos" || cat == "limpieza" || cat == "bebidas")){
+			cat = prompt("Ingrese una categoria valida: (almacen, lácteos, limpieza o bebidas)")
+		}
+		else {
+			if (prec < 0 || prec > 1000){
+				prec = parseInt(prompt("Ingrese un precio valido: (no puede ser menor a $0 ni mayor a $1000)"));
+			}
+			else {
+				prec = precMax > 
+				if (cat == "bebidas"){
+					cantBeb = cantBeb + 1;
+				}
+			}
+		}
+
+
+
+
+		x = prompt("Quieres introducir otro artículo?").toLowerCase();
+
+	}
+
+
+
+	document.write("El artículo con mayor precio es: ");
+	document.write("El artículo con mayor precio de la categoría lácteos es: ");
+	document.write("La cantidad de artículos ingresados de categoría bebidas es: ");
+
 }
